36419b170b27c446377049f032c01c61abd736b5
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-07-15 14:52:46 -0400

    Creating main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Cola/Cola.hpp"
+#include "Caja.hpp"
+
+#define INTERVALO_ATENCION_MIN 10
+#define INTERVALO_ATENCION_MAX 15
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
