13957b0b87ff8acf3f04a6abc13182c5e58a1a20
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-07-15 15:35:44 -0400

    Main: Start to do the two queue

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@
 using namespace std;
 
 int main() {
+    srand(time(0));
+
+    const int intervaloAtencionTotal = 120 + (rand() % (140 - 120 + 1));
+
+    Caja caja1, caja2, caja3;
+
+    Cola<string> colaNombres;
+    Cola<string> colaApellidos;
     
     return 0;
 }
