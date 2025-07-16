966c83e17cacd6d4b5555f8afdb9703b67ed5eee
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-07-16 10:28:17 -0400

    Fix some bugs

--- Caja.hpp
+++ Caja.hpp
@@ -1,6 +1,6 @@
 /**
  * \file main.hpp
- * \brief Aplicaci&oacute;n de una cola con strings. Clase que representa una caja de atenci&oacute;n en un banco.
+ * \brief Aplicación de una cola con strings. Clase que representa una caja de atención en un banco.
  * \author Ana Laura Chenoweth Galaz, Georgina Salcido Valenzuela
  * \date 17/03/2024
  */
@@ -18,24 +18,24 @@ private:
     Cola<std::string> colaEspera; /**< Cola de espera de clientes */
     Cola<std::string> colaAtendidos;  /**< Cola de clientes atendidos */
     int tiempoEspera; /**< Tiempo de espera para el siguiente cliente */
-    int contadorAtencion; /**< Contador del tiempo de atenci&oacute;n actual */
+    int contadorAtencion; /**< Contador del tiempo de atención actual */
 
 public:
     /**
      * @brief Constructor de la clase Caja.
-     * Inicializa el tiempo de espera y el contador de atenci&oacute;n.
+     * Inicializa el tiempo de espera y el contador de atención.
      */
     Caja() {
         tiempoEspera = rand() % INTERVALO;
         contadorAtencion = 0;
     }
     /**
-     * @brief M&eacute;todo para atender al cliente actual en la caja.
+     * @brief Método para atender al cliente actual en la caja.
      */
     void atenderCliente() {
         if (!colaEspera.EstaVacia()) {
-            std::cout << "\tCaja atendiendo a " << colaEspera.ObtenerFrente() << "\t\t";
-            std::cout << "Tiempo de atenci&oacute;n: " << contadorAtencion << "\n\n";
+            std::cout << "Caja atendiendo a " << colaEspera.ObtenerFrente() << "\t\t";
+            std::cout << "Tiempo de atención: " << contadorAtencion << "\n\n";
             contadorAtencion++;
             if (contadorAtencion >= colaEspera.ObtenerFrente().length()) {
                 colaAtendidos.Agregar(colaEspera.ObtenerFrente());
@@ -48,7 +48,7 @@ public:
         }
     }
     /**
-     * @brief M&eacute;todo para agregar un cliente a la cola de espera de la caja.
+     * @brief Método para agregar un cliente a la cola de espera de la caja.
      * @param nombreCompleto Nombre completo del cliente a agregar.
      * @param tiempoActual Tiempo actual para calcular el tiempo de espera.
      */
@@ -59,21 +59,21 @@ public:
         }
     }
     /**
-     * @brief M&eacute;todo para imprimir la cola de espera.
+     * @brief Método para imprimir la cola de espera.
      */
     void imprimirEspera() {
         if (!colaEspera.EstaVacia())
             colaEspera.ImprimirSinFrente();
     }
     /**
-     * @brief M&eacute;todo para imprimir la cola de clientes atendidos.
+     * @brief Método para imprimir la cola de clientes atendidos.
      */
     void imprimirAtendidos() {
         if (!colaAtendidos.EstaVacia())
             colaAtendidos.Imprimir();
     }
     /**
-     * @brief M&eacute;todo para obtener el tama�o de la cola de espera.
+     * @brief Método para obtener el tama�o de la cola de espera.
      * @return Tama�o de la cola de espera.
      */
     int obtenerTamEspera() {
@@ -84,7 +84,7 @@ public:
         return colaAtendidos.ObtenerTam();
     }
     /**
-     * @brief M&eacute;todo para obtener el primer elemento de la cola de espera.
+     * @brief Método para obtener el primer elemento de la cola de espera.
      * @return Primer elemento de la cola de espera.
      */
     std::string obtenerFrenteEspera() {
@@ -92,9 +92,9 @@ public:
     }
 };
 /**
- * @brief M&eacute;todo para cargar datos desde un archivo a una cola.
- * @param cola Cola donde se cargar&aacute;n los datos.
- * @param archivo Nombre del archivo del que se cargar&aacute;n los datos.
+ * @brief Método para cargar datos desde un archivo a una cola.
+ * @param cola Cola donde se cargarán los datos.
+ * @param archivo Nombre del archivo del que se cargarán los datos.
  */
 void cargarDatosEnCola(Cola<std::string>& cola, const std::string& archivo) {
     std::ifstream file(archivo);
